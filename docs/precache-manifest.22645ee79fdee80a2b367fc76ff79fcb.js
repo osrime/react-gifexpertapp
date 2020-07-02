9c0d50af53ac9f6f687617aca07523310d8475bb
Osvaldo <osrime@gmail.com>
2020-07-01 22:31:33 -0500

    Doc commit

---
+++ docs/precache-manifest.22645ee79fdee80a2b367fc76ff79fcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e3017e74fa61f4d3a3eddd4290ec6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd2f6187c63f19ccbdbb",
+    "url": "/static/css/main.3d259e8b.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd2f6187c63f19ccbdbb",
+    "url": "/static/js/main.62fdf909.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
